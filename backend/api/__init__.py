116e78c7da79be67e1864ee6e7b0425117a672e0
Maycon Almeida <mayconepaloma2016@gmail.com>
2025-05-30 12:07:56 -0300

    feat: add cors configuration at backend

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from api.routes.healthcheck import healthcheck_bp
 from api.routes.users import users_bp
 from api.routes.costs import costs_bp
@@ -6,6 +6,15 @@ from api.routes.receivements import receivements_bp
 
 
 app = Flask(__name__)
+
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        return response
 
 app.register_blueprint(healthcheck_bp, url_prefix="/api")
 app.register_blueprint(users_bp, url_prefix="/api")
